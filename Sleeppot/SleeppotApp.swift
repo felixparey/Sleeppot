20c5fcd6275a50105d677d46358b255a08b7ee65
felixparey <mysteryparey@gmail.com>
2023-10-19 20:51:14 +0200

    Initial Commit

---
+++ Sleeppot/SleeppotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleeppotApp.swift
+//  Sleeppot
+//
+//  Created by Felix Parey on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SleeppotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

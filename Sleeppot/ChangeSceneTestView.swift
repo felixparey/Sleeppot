de18a6530552384282052343be449f0fa67123ce
felixparey <mysteryparey@gmail.com>
2023-10-22 22:50:44 +0200

    Kind Of works (no transition)

---
+++ Sleeppot/ChangeSceneTestView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeSceneTestView.swift
+//  Sleeppot
+//
+//  Created by Felix Parey on 22/10/23.
+//
+
+import SwiftUI
+
+struct ChangeSceneTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeSceneTestView()
+}
---
+++ Sleeppot/SpriteViews.swift
@@ -0,0 +1,18 @@
+//
+//  SpriteViews.swift
+//  Sleeppot
+//
+//  Created by Felix Parey on 22/10/23.
+//
+
+import SwiftUI
+
+struct SpriteViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpriteViews()
+}

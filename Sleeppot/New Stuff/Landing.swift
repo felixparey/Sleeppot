20927b9fd1ba0e4a8206b3b244a016e57153b104
felixparey <mysteryparey@gmail.com>
2023-10-31 00:48:37 +0100

    Added lanfing page

---
+++ Sleeppot/New Stuff/Landing.swift
@@ -0,0 +1,18 @@
+//
+//  Landing.swift
+//  Sleeppot
+//
+//  Created by Felix Parey on 30/10/23.
+//
+
+import SwiftUI
+
+struct Landing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Landing()
+}
---
+++ Sleeppot/New Stuff/LandingPageView.swift
@@ -0,0 +1,8 @@
+//
+//  LandingPageView.swift
+//  Sleeppot
+//
+//  Created by Felix Parey on 30/10/23.
+//
+
+import Foundation
